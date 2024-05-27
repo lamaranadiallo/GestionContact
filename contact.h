dcba0ed44059430f22ef34cb435ddd9fb9e1028f
Lamarana <mrdiallo77@gmail.com>
2024-05-27 17:11:07 +0200

    First commit

---
+++ contact.h
@@ -0,0 +1,22 @@
+
+#ifndef __CONTACT__
+
+#define __CONTACT__
+#include<string>
+
+using namespace std;
+
+class Contact {
+public:
+    string nom;
+    string numero;
+
+    Contact(string nom, string numero) {
+        this->nom = nom;
+        this->numero = numero;
+    }
+};
+
+
+
+#endif // __CONTACT__
